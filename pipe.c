40986f2ff3a347d9ecec2097fca7d211359ad569
Jack <jack74387@gmail.com>
2025-03-19 11:45:54 +0800

    add command line argument

--- pipe.c
+++ pipe.c
@@ -3,39 +3,66 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <string.h>
 
-int main() {
+#define MAX_ARGS 10 
+
+void parse_command(char *command, char *argv[]) {
+    char *token = strtok(command, " ");
+    int i = 0;
+    while (token != NULL && i < MAX_ARGS - 1) {
+        argv[i++] = token;
+        token = strtok(NULL, " ");
+    }
+    argv[i] = NULL; // execvp requires NULL termination
+}
+
+int main(int argc, char *argv[]) {
     int pipefd[2]; // Pipe file descriptors
-    pid_t pid;
+    pid_t pid1, pid2;
 
-    if (pipe(pipefd) == -1) { // Create pipe
+    if (argc < 3) {  
+        fprintf(stderr, "Usage: %s <command1> <command2>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    if (pipe(pipefd) == -1) { 
         perror("pipe");
         exit(EXIT_FAILURE);
     }
 
-    pid = fork();
-    if (pid < 0) {
+    // First fork: Run command1
+    pid1 = fork();
+    if (pid1 < 0) {
         perror("fork");
         exit(EXIT_FAILURE);
     }
-
-    if (pid == 0) { 
-        // Child process executes `wc -l`
-        close(pipefd[1]); // Close unused write end
-        dup2(pipefd[0], STDIN_FILENO); // Redirect stdin to pipe read end
-        close(pipefd[0]); // Close original read end
-        execlp("wc", "wc", "-l", NULL); // Execute `wc -l`
-        perror("execlp");
-        exit(EXIT_FAILURE);
-    } else {
-        // Parent process executes `ls`
+    if (pid1 == 0) { 
+        // Child 1 executes command1
         close(pipefd[0]); // Close unused read end
         dup2(pipefd[1], STDOUT_FILENO); // Redirect stdout to pipe write end
-        close(pipefd[1]); // Close original write end
-        execlp("ls", "ls", NULL); // Execute `ls`
-        perror("execlp");
+        close(pipefd[1]); // Close write end after redirection
+
+        char *cmd1[MAX_ARGS]; 
+        parse_command(argv[1], cmd1); 
+        execvp(cmd1[0], cmd1);
+        perror("execvp");
         exit(EXIT_FAILURE);
     }
+
+    // **Parent Process run command2
+    close(pipefd[1]); // Close unused write end
+    waitpid(pid1, NULL, 0); // wait child process
+
+    dup2(pipefd[0], STDIN_FILENO); // Redirect stdin to pipe read end
+    close(pipefd[0]); // Close unused read end
+
+    char *cmd2[MAX_ARGS]; 
+    parse_command(argv[2], cmd2); 
+    execvp(cmd2[0], cmd2);
+    
+    perror("execvp");
+    exit(EXIT_FAILURE);
 
     return 0;
 }
